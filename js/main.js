d9939f21f675a155f9a4eef06400c135b6bc0d4c
Jeremy Verdolino <j.verdolino@gmail.com>
2017-06-22 12:29:26 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You have a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length===2) {
+  checkForMatch();
+  } 
+};
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!");
-} else {
-  alert("sorry,try again");
-}
-  
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(1);
