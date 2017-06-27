09b0cf16f430d282895ad4667c3396a176a945d5
jayverts <j.verdolino@gmail.com>
2017-06-27 17:24:04 -0600

    final update

--- js/main.js
+++ js/main.js
@@ -29,12 +29,12 @@ var checkForMatch = function () {
 	}
 };
 var flipCard = function () {
-	var cardId = this.getAttribute(data-id);
-	console.log("User flipped " + cards[cardId].rank);
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
+  console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-  	this.setAttribute("src", cards[cardId.cardImage]);
   if (cardsInPlay.length===2) {
   checkForMatch();
   } 
@@ -44,7 +44,7 @@ var createBoard = function() {
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement("img");
 		cardElement.setAttribute("src", "images/back.png");
-		cardElement.setAttribute("data-id" + i);
+		cardElement.setAttribute("data-id", i);
 		cardElement.addEventListener('click', flipCard);
     	document.getElementById('game-board').appendChild(cardElement);
 	}
